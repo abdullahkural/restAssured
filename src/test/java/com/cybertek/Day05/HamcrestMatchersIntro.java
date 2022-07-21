c2f31267acaa2176db68381bfcf7740de5ea1e12
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-21 14:06:28 +0200

    Day05 Hamcrest Matchers Intro first Matcher assertions added

--- src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
@@ -4,12 +4,30 @@ package com.cybertek.Day05;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
 
 public class HamcrestMatchersIntro {
 
     @Test
     public void simpleTest1(){
 
+        //actual 5+5
         MatcherAssert.assertThat(5+5, Matchers.is(10));
+        assertThat(5+5, Matchers.is(10));
+        assertThat(5+5, Matchers.equalTo(10));
+        assertThat(5+5, Matchers.is(Matchers.equalTo(10)));
+
+        /*
+        The instructor asserted like:
+        assertThat(5+5, is(10));
+        assertThat(5+5, equalTo(10));
+        assertThat(5+5, is(equalTo(10)));
+
+        But I could not. I can not import
+         */
+
     }
 }
