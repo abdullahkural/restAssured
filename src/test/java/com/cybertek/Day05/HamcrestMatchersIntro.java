06e8f0dabd53b938465a6a5d013f05963f6efecc
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-21 13:38:05 +0200

    Day05 Hamcrest Matchers Intro first Matcher assertion

--- src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
@@ -1,5 +1,8 @@
 package com.cybertek.Day05;
 
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class HamcrestMatchersIntro {
@@ -7,5 +10,6 @@ public class HamcrestMatchersIntro {
     @Test
     public void simpleTest1(){
 
+        MatcherAssert.assertThat(5+5, Matchers.is(10));
     }
 }
