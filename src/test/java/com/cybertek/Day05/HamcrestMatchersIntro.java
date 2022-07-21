8e41f2ce052f0b081f5abd2b8b6ab85549e3eee7
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-21 14:19:01 +0200

    Day05 Hamcrest Matchers Intro first Matcher String assertions

--- src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day05/HamcrestMatchersIntro.java
@@ -3,6 +3,7 @@ package com.cybertek.Day05;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -11,6 +12,7 @@ import static org.hamcrest.Matchers.*;
 
 public class HamcrestMatchersIntro {
 
+    @DisplayName("Assertions with numbers")
     @Test
     public void simpleTest1(){
 
@@ -31,11 +33,42 @@ public class HamcrestMatchersIntro {
         assertThat(5+5, is(not(9)));
         assertThat(5+5, is(not(equalTo(9))));
 
-        //number comparison
+        /*
+        number comparison
+        greaterThan()
+        greaterThanOrEqualTo()
+        lessThan()
+        lessThanOrEqualTo()
+         */
 
+        assertThat(5+5, is(greaterThan(9)));
+        assertThat(5+5,is(greaterThanOrEqualTo(10)));
+        assertThat(5+5,is(lessThan(11)));
+        assertThat(5+5,is(lessThanOrEqualTo(11)));
 
+    }
 
+    @DisplayName("Assertions with String")
+    @Test
+    public void stringHamcrest(){
 
+        String text = "I will earn too much";
+
+        //checking equality is same as numbers
+        assertThat(text, is("I will earn too much"));
+        assertThat(text, equalTo("I will earn too much"));
+        assertThat(text, is(equalTo("I will earn too much")));
+
+        //check if this starts with "I"
+        assertThat(text, startsWith("I"));
+        //now do it in case insensetive
+        assertThat(text, startsWithIgnoringCase("i"));
+
+        //ends with
+        assertThat(text, endsWith("much"));
+        assertThat(text,endsWithIgnoringCase("MuCh"));
+
+        
 
     }
 }
