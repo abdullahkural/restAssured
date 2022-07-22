68cb2709e2dbbb9e0c7c882425b2c3ff94b71b0b
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-22 21:21:24 +0200

    Day05 SpartanHamcrestTest class test1

--- src/test/java/com/cybertek/Day05/SpartanHamcrestTest.java
+++ src/test/java/com/cybertek/Day05/SpartanHamcrestTest.java
@@ -1,4 +1,43 @@
 package com.cybertek.Day05;
 
-public class SpartanHamcrestTest {
+import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInRelativeOrder;
+
+public class SpartanHamcrestTest extends SpartanTestBase {
+
+    @DisplayName("GET Spartan/search and chaining together")
+    @Test
+    public void test1(){
+
+        //ALONG WITH THIS STATEMENT, I WANT TO SAVE NAMES INSIDE THE LIST OF STRING
+        List <String> names  = given().accept(ContentType.JSON)
+                            .and().queryParams("nameContains", "j", "gender", "Male")
+                            .when()
+                            .get(baseURI + "/api/spartans/search")
+                            .then()
+                            .statusCode(200)
+                            .and()
+                            .body("totalElement", is(8))
+                            .extract().response().jsonPath().getList("content.name");
+
+        //.extract().response().jsonPath().getList("content.name") --> saves names inside a list
+        //
+
+        System.out.println(names);
+    }
+
+
+
 }
