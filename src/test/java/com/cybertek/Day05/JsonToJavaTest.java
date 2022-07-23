4626c5c58cf78024ea395fba46a24187c0c87afe
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-23 14:15:09 +0200

    Day05 SpartanHamcrestTest JsonToJavaTest class created jackson Databind library added to POM.xml

--- src/test/java/com/cybertek/Day05/JsonToJavaTest.java
+++ src/test/java/com/cybertek/Day05/JsonToJavaTest.java
@@ -10,6 +10,7 @@ import static io.restassured.RestAssured.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -20,6 +21,14 @@ public class JsonToJavaTest extends SpartanTestBase {
     @DisplayName("GET one Spartan and deserialize to Map")
     @Test
     public void oneSpartanToMap(){
+
+        Response response = given().pathParam("id", 15)
+                .when().get(baseURI + "/api/spartans/{id}")
+                .then().statusCode(200).extract().response();
+
+        //get the json and convert it to the map
+
+        Map <String, Object> jasonMap = response.as(Map.class);
 
     }
 
