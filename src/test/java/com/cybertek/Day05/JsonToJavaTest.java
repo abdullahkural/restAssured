51c60168a41fb42224031cec0bc7523694367392
Abdullah Kural <portreciziyorum@gmail.com>
2022-07-23 14:06:43 +0200

    Day05 SpartanHamcrestTest JsonToJavaTest class created

---
+++ src/test/java/com/cybertek/Day05/JsonToJavaTest.java
@@ -0,0 +1,30 @@
+package com.cybertek.Day05;
+
+import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInRelativeOrder;
+
+public class JsonToJavaTest extends SpartanTestBase {
+
+    @DisplayName("GET one Spartan and deserialize to Map")
+    @Test
+    public void oneSpartanToMap(){
+
+    }
+
+
+
+
+
+}
